5be056094c02645fc4b7d52442fbfd14873917ff
Shaun Dawson <23201454+shaundawson@users.noreply.github.com>
2022-07-08 17:20:47 -0400

    save list

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# web_scraper
+# A Web Scraper with Python - 100 Movies that You Must Watch
+This python script scrapes HTML data from an Empire Online article that lists the top 100 movies of all time. Requests takes the data from the website and Beautiful Soup inteprets the data. Then the script writes the list of the top 100 movies using Panadas into a CSV and text file.
+
+###### Resources
+- Beautiful Soup Documentation: 
+https://www.crummy.com/software/BeautifulSoup/bs4/doc
+- The 100 Greatest Movies - Empire Online:
+https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/
  
--- main.py
+++ main.py
@@ -1,25 +1,24 @@
+from operator import index
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd
 
-response = requests.get("https://news.ycombinator.com/")
-page = response.text
+response = requests.get("https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/")
+content = response.text
 
-soup = BeautifulSoup(page,"html.parser")
-articles = soup.find_all(name="a", class_="titlelink")
+soup = BeautifulSoup(content,"html.parser")
+all_movies = soup.find_all(name="h3", class_="title")
+movie_list = []
 
-article_texts = []
-article_links = []
+for movie in all_movies:
+    title = movie.getText()
+    movie_list.append(title)
 
-for article_tag in articles:
-        article_text = article_tag.getText()
-        article_texts.append(article_text)
-        article_link = article_tag.get("href")
-        article_links.append(article_link)
-    
-article_upvotes = [score.getText() for score in soup.find_all(name="span", class_="score")]
+movie_list.reverse()
 
-
-
-print(article_texts)
-print(article_links)
-print(int(article_upvotes[0].split()[0]))
+with open('movies.txt', 'w') as f:
+    for m in movie_list:
+        f.write(m + '\n')
+        
+df = pd.read_fwf('movies.txt')
+df.to_csv('movies.csv', index=False)
